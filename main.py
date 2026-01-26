8ea97d031cd661f3d8e410b72f399660ccc7bc15
consultantds-maker <consultantds@saicjs.com>
2026-01-26 18:35:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ main_page = st.Page("main.py", title="Main Page", icon="🎈")
 main_page = st.Page(r"main.py", title="Main Page", icon="🎈")
 page_1 = st.Page(r"tire1.py", title="Discover Suraksha Lens with us", icon="❄️")
 
-page_4 = st.Page(r"E:\suraksha_lens\dashboard\tire4.py", title="Climate Exploitation Risk Index (CERI)", icon="❄️")
+page_4 = st.Page(r"tire4.py", title="Climate Exploitation Risk Index (CERI)", icon="❄️")
 
 
 # Set up navigation
@@ -24,4 +24,5 @@ pg = st.navigation([main_page,page_1,page_4])
 
 # Run the selected page
 pg.run()
+
 
