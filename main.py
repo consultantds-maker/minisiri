f9e9b1319dd0a123c70ca4bd514011d69ed351dd
consultantds-maker <consultantds@saicjs.com>
2026-01-26 15:17:17 +0530

    Refactor page definitions and navigation setup

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import streamlit as st
 main_page = st.Page("main.py", title="Main Page", icon="🎈")
 page_1 = st.Page(r"tire1.py", title="Tire 1", icon="❄️")
 page_2 = st.Page(r"tire2.py", title="Tire 2", icon="❄️")
-page_3 = st.Page(r"E:\suraksha_lens\dashboard\tire3.py", title="Tire 3", icon="❄️")
-page_4 = st.Page(r"E:\suraksha_lens\dashboard\tire4.py", title="Tire 4", icon="❄️")
+page_3 = st.Page(r"tire3.py", title="Tire 3", icon="❄️")
+page_4 = st.Page(r"tire4.py", title="Tire 4", icon="❄️")
 
 # Set up navigation
 pg = st.navigation([main_page,page_1, page_2, page_3,page_4])
@@ -20,3 +20,4 @@ pg = st.navigation([main_page,page_1, page_2, page_3,page_4])
 # Run the selected page
 
 pg.run()
+
